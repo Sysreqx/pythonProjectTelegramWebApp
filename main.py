d039efb5ea12ce4a7eae8c45041d8167dc2fffc8
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-10-06 16:17:31 +0600

    save me please

--- main.py
+++ main.py
@@ -11,20 +11,21 @@ import pyzbar.pyzbar as pyzbar
 
 import os
 import requests
-from PIL import Image 
+from PIL import Image
+
 
 def detect():
     camera = cv2.VideoCapture(0)
     data = ''
     while True:
-        ret, frame = camera.read() 
-        barcodes = pyzbar.decode(frame) 
+        ret, frame = camera.read()
+        barcodes = pyzbar.decode(frame)
         data = ''
         for barcode in barcodes:
             data = barcode.data.decode('utf-8')
         if data != '':
-            with open('data.txt', 'w') as file:
-                file.write(data)
+            # with open('../data.txt', 'w') as file:
+            #     file.write(data)
             break
         if cv2.waitKey(1) == ord('q'):
             break
@@ -32,17 +33,18 @@ def detect():
     camera.release()
     return data
 
-def scan_image():
-    image = "barcode1.jpg"
+
+def scan_image(image):
     barcodes = pyzbar.decode(Image.open(image))
     data = ''
     for barcode in barcodes:
         data = barcode.data.decode('utf-8')
-    if data != '':
-        with open('data.txt', 'w') as file:
-            file.write(data)
+    # if data != '':
+    # with open('../data.txt', 'w') as file:
+    #     file.write(data)
 
     return data
+
 
 app = FastAPI()
 
@@ -55,25 +57,24 @@ templates = Jinja2Templates(directory="templates")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/scanning")
 async def scan():
-    data=detect()
-    return {"barcode":data}
+    data = detect()
+    return {"barcode": data}
 
 
 # image save
 @app.post("/uploadfiles/")
 async def create_upload_files(files: list[UploadFile]):
-
     for file in files:
         with open(f'{file.filename}', 'wb') as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-    return {"barcode":scan_image()}
+    return {"barcode": scan_image(file.filename)}
 
 # # image read barcode
 # @app.get("/r")
 # async def scan():
 #     data=scan_image()
 #     return {"barcode":data}
-
